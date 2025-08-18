cf852ae5ee1b8545142f10f5f828c4ed12ad0e7f
Ariel Benichou <videojr3000@gmail.com>
2025-08-18 19:42:02 +0300

    add mem.repeatIntoSlice

--- src/mem.zig
+++ src/mem.zig
@@ -1,4 +1,33 @@
+const std = @import("std");
+const assert = std.debug.assert;
+const testing = std.testing;
+
 pub const bytes_per_kilobyte = 1024;
 pub const bytes_per_megabyte = bytes_per_kilobyte * 1024;
 pub const bytes_per_gigabyte = bytes_per_megabyte * 1024;
 pub const bytes_per_terabyte = bytes_per_gigabyte * 1024;
+
+/// Repeats the pattern into the data slice.
+/// Assert that the data slice is a multiple of the pattern length.
+/// Assert that the pattern is not empty.
+/// Assert that the data slice is not empty.
+pub fn repeatIntoSlice(comptime T: type, data: []T, pattern: []const T) void {
+    assert(data.len != 0);
+    assert(pattern.len != 0);
+    assert(@mod(data.len, pattern.len) == 0);
+
+    for (data, 0..) |*dst, i| {
+        const pattern_index = i % pattern.len;
+        dst.* = pattern[pattern_index];
+    }
+}
+
+test repeatIntoSlice {
+    var data: [15]u8 = std.mem.zeroes([15]u8);
+    repeatIntoSlice(u8, data[0..9], &[_]u8{ 1, 2, 3 });
+    try testing.expectEqualSlices(
+        u8,
+        &[_]u8{ 1, 2, 3, 1, 2, 3, 1, 2, 3, 0, 0, 0, 0, 0, 0 },
+        &data,
+    );
+}
