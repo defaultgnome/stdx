40784047a7cb971424d2fb2eafa071ea94e5bdf9
Ariel Benichou <videojr3000@gmail.com>
2025-08-19 23:00:58 +0300

    add copy2dSlice

--- src/mem.zig
+++ src/mem.zig
@@ -31,3 +31,52 @@ test repeatIntoSlice {
         &data,
     );
 }
+
+/// copy a window from `src` into a matching window in `dest`.
+pub fn copy2dSlice(
+    comptime T: type,
+    dest: []T,
+    dest_width: usize,
+    dest_x: usize,
+    dest_y: usize,
+    src: []T,
+    src_width: usize,
+    src_height: usize,
+    src_x: usize,
+    src_y: usize,
+) void {
+    for (0..src_height) |y| {
+        const dest_row_start = (dest_y + y) * dest_width + dest_x;
+        const src_row_start = (src_y + y) * src_width + src_x;
+        @memcpy(dest[dest_row_start .. dest_row_start + src_width], src[src_row_start .. src_row_start + src_width]);
+    }
+}
+
+test copy2dSlice {
+    var dest: [100]u8 = undefined;
+    const dest_width = 10;
+    const dest_x = 2;
+    const dest_y = 3;
+    const src_width = 3;
+    const src_height = 3;
+    var src: [src_width * src_height]u8 = undefined;
+    for (0..src.len) |i| {
+        src[i] = @intCast(i + 1);
+    }
+    copy2dSlice(
+        u8,
+        &dest,
+        dest_width,
+        dest_x,
+        dest_y,
+        &src,
+        src_width,
+        src_height,
+        0,
+        0,
+    );
+
+    try testing.expectEqualSlices(u8, &.{ 1, 2, 3 }, dest[32..35]);
+    try testing.expectEqualSlices(u8, &.{ 4, 5, 6 }, dest[42..45]);
+    try testing.expectEqualSlices(u8, &.{ 7, 8, 9 }, dest[52..55]);
+}
